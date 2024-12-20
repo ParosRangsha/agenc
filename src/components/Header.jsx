53387918a477e640328f4d4bb8dc048bfc0a65c1
paros <paroscrangsha@gmail.com>
2024-12-20 11:17:50 +0600

    navbar radius

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
         setshow(!show)
     }
   return (
-    <div className="header w-full fixed top-0 left-0 bg-[#555] lg:px-[0px] px-[10px]">
+    <div className="header w-full fixed top-0 left-0 lg:px-[0px] px-[10px]">
         <div className="contain  max-w-container mx-auto flex py-[10px] items-center">
             <div className="logo w-[30%]">
                 <img src={logo} alt="logo" />
             </div>
             <div className="menus w-[60%]">
-                <ul className={`absolute top-[100%] right-0 w-[30%] bg-[#555] h-[100vh] transition duration-[1s] ${show ?  'right-0': 'right-[-100%]'}`}>
+                <ul className={`absolute top-[100%] right-0 w-[20%] bg-[#555] h-[100vh] transition duration-[1s] ${show ?  'right-0': 'right-[-100%]'}`} style={{borderRadius:'10px 0 0 10px'}}>
                     <li>
                         <Link to='/' className='lg:py-[15px] py-[5px] block lg:px-[42px] text-[18px] font-bold text-[#fff] text-center'>Home</Link>
                     </li>
@@ -36,7 +36,7 @@ const Header = () => {
                     </li>
                 </ul>
             </div>
-            <div className='text-[#fff]'>
+            <div className='text-[#fff] p-[10px] bg-[#555]'>
                 {show? <GiCrossedBones onClick={handleshow}/> : <FaBars onClick={handleshow}/>}
             </div>
         </div>
